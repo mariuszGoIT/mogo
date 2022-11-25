4ac71c8de8825648de954ac9b50f0fd082f3cf84
Mariusz <pl.rybson@gmail.com>
2022-11-25 20:24:15 +0100

    refactor

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const naviagionElement = document.querySelector(".nav");
+const headerElement = document.querySelector(".header");
 const menuButtonElement = document.querySelector(".header__menu-button");
 const menuElements = document.querySelectorAll(".nav__link");
 
-menuButtonElement.addEventListener("click", () =>
-  naviagionElement.classList.toggle("nav-open")
-);
+menuButtonElement.addEventListener("click", () => {
+  headerElement.classList.toggle("nav-open");
+});
 
 menuElements.forEach((element) => {
   element.addEventListener("click", () =>
-    naviagionElement.classList.remove("open-nav")
+    headerElement.classList.remove("nav-open")
   );
 });
